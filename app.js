b643ec882c9339218aa1d021de6b4bc6af2cdf3d
qreoct <9080974+qreoct@users.noreply.github.com>
2021-06-21 14:53:59 +0800

    https redirect test

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error('error connection to MongoDB:', error.message)
   })
 
+// force https on heroku
+// https://jaketrent.com/post/https-redirect-node-heroku
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next()
+    }
+  })
+}
+
 app.use('/', express.static('build')) // look in the build directory to serve frontend
 
 app.use(cors())
